ca6d6fb08ed2e0091b62d8aff660ed6f8043890b
eanugent <eanugent@users.noreply.github.com>
2018-09-17 03:03:40 -0400

    Began Screen Mockup

---
+++ Activote/Controllers/ActionController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Activote.Models;
+
+namespace Activote.Controllers
+{
+    [AuthorizeAttribute()]
+    public class ActionController : Controller
+    {
+        private activoteEntities db = new activoteEntities();
+        // GET: Action
+        public ActionResult _Registered()
+        {
+            return View();
+        }
+
+        public ActionResult _InitRegistered()
+        {
+            return PartialView();
+        }
+
+        public ActionResult _CheckRegistration()
+        {
+            return PartialView(new activoteEntities().States.OrderBy(s => s.StateName).ToList());
+        }
+
+        public ActionResult _UploadImageView()
+        {
+            TrackedGUIDAccess.TrackedActionCompleted();
+            return PartialView();
+        }
+
+        public ActionResult _ChooseFrame(string actionTag)
+        {
+            return PartialView(db.Frames.Where(f => f.Action.ActionTag == actionTag).ToList());
+        }
+    }
+}
